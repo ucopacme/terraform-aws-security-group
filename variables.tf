3666292d1e5961cd36d809097217c3339df91c16
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-10-27 11:59:26 -0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "name" {
+  description = "The name of the security group"
+  default     = null
+}
+variable "enabled" {
+  type        = bool
+  default     = true
+  description = "Whether to create Security Group."
+}
+variable "description" {
+  description = "The Security Group description."
+  default     = "Managed by Terraform"
+}
+
+variable "ingress" {
+    description = "Ingress rules for security group"
+    type        = any
+    default     = []
+}
+
+variable "egress" {
+    description = "Egress rules for security group"
+    type        = any
+    default     = []
+}
+
+variable "revoke_rules_on_delete" {
+  description = "Instruct Terraform to revoke all of the Security Groups attached ingress and egress rules before deleting the rule itself"
+  type        = bool
+  default     = false
+}
+
+variable "vpc_id" {
+    description = "The VPC ID"
+    default     = null
+}
+
+variable "tags" {
+    description = "A mapping of tags to assign to the resource."
+    type        = map(string)
+    default     = {}
+}
